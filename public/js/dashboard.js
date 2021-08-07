2f64493afbfea2e8a072728ef33908bd0c9c9c24
Caitlin Swickard <caitlin6029@gmail.com>
2021-08-06 18:01:29 -0600

    update to blog route

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,10 +2,10 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector('#blog-name').value.trim();
-  const description = document.querySelector('#blog-desc').value.trim();
+  const content = document.querySelector('#blog-desc').value.trim();
 
-  if (title  && description) {
-    const response = await fetch(`/api/dashboards`, {
+  if (title && content) {
+    const response = await fetch(`/api/blogs`, {
       method: 'POST',
       body: JSON.stringify({ title, content }),
       headers: {

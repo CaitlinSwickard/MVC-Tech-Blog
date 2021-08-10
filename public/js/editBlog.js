adcc163ade86b856f4eeca22439e42eb5ac9340d
Caitlin Swickard <caitlin6029@gmail.com>
2021-08-09 23:10:08 -0600

    added update/delete function

--- public/js/editBlog.js
+++ public/js/editBlog.js
@@ -1,34 +1,36 @@
-// async function editFormHandler(event) {
-//   event.preventDefault();
+async function editFormHandler(event) {
+  event.preventDefault();
 
-//   // get the post id from the url
-//   const id = window.location.toString().split('/')[
-//       window.location.toString().split('/').length - 1
-//   ];
+  // get the blog id from the url
+  // const id = window.location.toString().split('/')[
+  //     window.location.toString().split('/').length - 1
+  // ];
 
-//   // Get the post title and post text from the form
-//   const title = document.querySelector('input[name="post-title"]').value;
-//   const content = document.querySelector('textarea[name="post-text"]').value;
+  // if (event.target.hasAttribute('data-id')) {
+  //   const id = event.target.getAttribute('data-id')
 
-//   // use the update route to update the post
-//   const response = await fetch(`/api/blogs/${id}`, {
-//       method: 'PUT',
-//       body: JSON.stringify({
-//         title,
-//         content
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     });
-//   // if the edit action is successful, redirect to the dashboard page, otherwise display the error
-//   if (response.ok) {
-//       document.location.replace('/dashboard');
-//       // otherwise, display the error
-//       } else {
-//       alert(response.statusText);
-//       }
+  // Get the post title and post text from the form
+  const title = document.querySelector("#blog-name").value;
+  const content = document.querySelector("#blog-desc").value;
 
-// }
+  // use the update route to update the post
+  const response = await fetch(`/api/blogs/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  // if the edit action is successful, redirect to the dashboard page, otherwise display the error
+  if (response.ok) {
+      document.location.replace('/dashboard');
+      } else {
+      alert(response.statusText);
+      }
 
-// document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+}
+
+document.querySelector('.edit-blog-form').addEventListener('submit', editFormHandler);
